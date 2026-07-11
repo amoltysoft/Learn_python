e565ab0d1ab2cd182196fe57f72f740db9a8b274
amoltysoft <amalalwarithy@gmail.com>
2026-07-11 18:08:36 +0300

    add arithmetic operator

---
+++ arithmetic_operator.py
@@ -0,0 +1,33 @@
+#------------------------------------------
+#--------- Arithmetic operator -----------
+# additoin => +
+# subtraction => -
+# multiplition => *
+# Division => /
+# modulus => %
+# exponent => **
+# floor division => //
+
+
+#EXMPLES
+num1 = 7
+num2 = 4
+# addition
+print (f"{num1}+{num2}={num1+num2}") # 11
+
+# subtraction
+print (f"{num1}-{num2}={num1-num2}") # 3
+
+# multiplition
+print (f"{num1}x{num2}={num1*num2}") # 28
+
+# division 
+print (f"{num1}÷{num2}={num1/num2}") # 1.75
+# modulus 
+print (f"{num1}%{num2}={num1%num2}") # 3
+# exponent
+print (f"{num1}**{num2}={num1**num2}") # 2401
+# floor Division
+print (f"{num1}//{num2}={num1//num2}") # 1
+
+print (f"{num1}//{num2}={float(num1//num2)}") # 1.0
